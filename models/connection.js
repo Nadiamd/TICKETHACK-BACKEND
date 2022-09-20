3b468928860a5966731acfefd5cb911ce6f7e46c
Nadia <nadiamahmoud.09@gmail.com>
2022-09-20 16:23:57 +0200

    test16H23

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://Nadia:Livia.2020@cluster0.dlxqx3c.mongodb.net/tickethack';
+const connectionString = 'mongodb+srv://Nadia:Livia.2020@cluster0.dlxqx3c.mongodb.net/TICKETHACK';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
--- routes/trips.js
+++ routes/trips.js
@@ -6,25 +6,29 @@ const Trip = require('../models/trips');
 
 //const OWM_API_KEY = 'ce7418650c86eae6629dfcfdda141c14';
 
+ //departure: { $regex: new RegExp(req.body.departure) , $options: "i" },
+    //arrival: { $regex: new RegExp(req.body.arrival) , $options : "i"},
+    //date: req.body.date,
 
 
 router.post('/formulaire', (req, res) => {
     //console.log(req.body.departure);
   Trip.find({
-    departure: { $regex: new RegExp(req.body.departure) , $options: "i" },
+     departure: { $regex: new RegExp(req.body.departure) , $options: "i" },
     arrival: { $regex: new RegExp(req.body.arrival) , $options : "i"},
     //date: req.body.date,
-  }).then((dbData) => {
+
+  }
+
+  ).then((dbData) => {
    console.log(dbData);
-    if (dbData) {
+    if (dbData && dbData.length > 0) {
       res.json({ result: true, trips: dbData });
     } else {
       res.json({ result: false, error: "Trip not found" });
     }
   })
 })
-
-
 
 
 
